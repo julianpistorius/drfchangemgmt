e16961c4268cca58510c1c442b80f5c1a2f75f8b
sassoo <scottmcclanahan2002@gmail.com>
2016-04-19 21:13:53 -0400

    comments fixes

--- drfchangemgmt/mixins.py
+++ drfchangemgmt/mixins.py
@@ -22,6 +22,7 @@ except (AttributeError, KeyError):
 
 def _get_changed_fields(model_instance):
     """ Return the changed fields data structure
+
     This will be bound to a model instance & should only ever
     be called from a model instance
     """
@@ -37,6 +38,7 @@ def _get_new_pk(instance):
 
 def _get_old_pk(instance, field_name):
     """ Given a model instance & field_name find the true name of the field
+
     This is needed to support the uncommon scenario where the
     instance's ForeignKey column name may have been overridden
     from the django default behavior of <field_name>_id.
@@ -50,22 +52,28 @@ def _get_old_pk(instance, field_name):
 
 class ChangeMgmtSerializerMixin(object):
     """ DRF Serializer mixin for added change management functionality
+
     The DRF serializer will add a new private property named
     `_changed_fields` & a public method named `get_changed_fields`
     to the instance provided.
+
     The `_changed_fields` property contains an empty dict if no
     changes were made & if changes are present then each key is
     named after the field & the value is:
+
         {
             old: <old value>
             new: <new value>
         }
+
     By default PrimaryKeyRelatedFields are optimized to compare
     only the ID's & the old/new values will be limited to the
     keys as well.
-    The DRF__CHANGEMGMT[PK_FAST_COMPARE] django settings can be
+
+    The DRF_CHANGEMGMT[PK_FAST_COMPARE] django settings can be
     set to False so full Model compares are performed causing
     a database query for the old value globally.
+
     INFO: Change tracking only occurs on the initial input of
           data from the requesting client against EXISTING models.
           New models are completely skipped & mutations after the
@@ -75,14 +83,17 @@ class ChangeMgmtSerializerMixin(object):
 
     def to_internal_value(self, data):
         """ Override the DRF native to_internal_value method
+
         Use the output from the native `to_internal_value` to
         automatically prune the list of fields to compare. It
         will only return fields that are both "writable" & provided
         by the requesting user.
+
         Additionally, it already takes care of the uncommon scenario
         of a field using the `source` argument where the backend
         object actually uses a different field than the API. That
         rename is already taken care of so we don't need to sweat it.
+
         INFO: Some helper methods are used but intentionally left
               off the mixin to avoid cluttering the parent serializer.
         """
@@ -92,9 +103,6 @@ class ChangeMgmtSerializerMixin(object):
         if not self.instance:
             return ret
 
-        # existing so carry on
-        fields = self.get_fields()
-
         # initialize the instance
         self.instance._changed_fields = {}
         self.instance.get_changed_fields = six.create_bound_method(
@@ -103,7 +111,7 @@ class ChangeMgmtSerializerMixin(object):
         )
 
         for key, val in ret.items():
-            field = fields[key].__class__
+            field = self.fields[key].__class__
 
             if isinstance(PrimaryKeyRelatedField, field) and PK_FAST_COMPARE:
                 old_val = _get_old_pk(self.instance, key)
